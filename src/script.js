47384a29cdd6abfce0c02d04f902df38e35d1310
Faid <faidwijaya@gmail.com>
2026-04-20 10:17:16 +0700

    add youtube embeded with hover play/pause

--- src/script.js
+++ src/script.js
@@ -17,3 +17,23 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// Video Control (Card 1)
+const videoIframe = document.querySelector("#video-iframe");
+const videoCard = videoIframe ? videoIframe.closest(".group") : null;
+
+if (videoIframe && videoCard) {
+  videoCard.addEventListener("mouseenter", () => {
+    videoIframe.contentWindow.postMessage(
+      '{"event":"command","func":"playVideo","args":""}',
+      "*"
+    );
+  });
+
+  videoCard.addEventListener("mouseleave", () => {
+    videoIframe.contentWindow.postMessage(
+      '{"event":"command","func":"pauseVideo","args":""}',
+      "*"
+    );
+  });
+}
